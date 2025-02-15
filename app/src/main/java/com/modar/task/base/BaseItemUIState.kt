5b68d92ec809ac15bae31234b5fe4ce23ec4216a
MostafaKader56 <mostafa.kader2350@gmail.com>
2025-02-15 04:02:18 +0200

    BaseItemUIState for the project

--- app/src/main/java/com/modar/task/base/BaseItemUIState.kt
+++ app/src/main/java/com/modar/task/base/BaseItemUIState.kt
@@ -2,6 +2,7 @@ package com.modar.task.base
 
 open class BaseItemUIState<out T : Any>(
     open val item: T?,
+    open val position: Int,
     open var success: Boolean = false,
     open var errorMessageRes: Int? = null
 )
